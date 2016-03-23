af7247b6b3232f4ce30bd7d9bafaef34bbf071a9
lrjbrual <lrjbrual@live.com>
2016-03-23 21:56:57 +0000

    Add devise and bootstrap for authentication and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   get 'welcome/index'
 
 
@@ -7,7 +8,6 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   #root 'welcome#index'
-  get 'index', to: 'welcome#index'
     root 'welcome#index'
   
 
